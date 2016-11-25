fbb3ccefa32d526242e6e3358b51b868d4d50acb
umesh.sharma <umesh.sharma@logicielsolutions.co.in>
2016-11-25 12:09:59 +0530

    simple add new file and create small model

--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('employees');
+  // this.route('employe');
 });
 
 export default Router;
---
+++ app/routes/employees/employe.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  // model(params){
+  //   return return [
+  //     {id:1,name:'sonu mourya'},
+  //     {id:2,name:'sunny'},
+  //     {id:3,name:'sam'},
+  //     {id:4,name:'suraj'},
+  //     {id:5,name:'sachin'},
+  //     {id:6,name:'sager'},
+  //     {id:7,name:'sulakhan'},
+  //     {id:8,name:'surinder'},
+  //     {id:9,name:'saroj'},
+  //     {id:10,name:'sikander'}
+  //   ].findBy('id',params.employe_id);
+  // }
+});
